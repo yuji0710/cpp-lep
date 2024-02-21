d080fb58b4a50eb2c0cf0744656d7b57e768f229
yuji0710 <142722535+yuji0710@users.noreply.github.com>
2024-02-21 14:28:02 +0530

    queue linklist

--- queue_linklist.cpp
+++ queue_linklist.cpp
@@ -57,11 +57,61 @@ void peek()
 
 }
 int main()
-{
-    create();
-    display();
-    del();
-    display();
-    peek();
+{ int w = 0;
+    while (w == 0)
+    {
+        cout << "1-> create link list \n";
+        cout << "2-> exit \n";
+        cout << " enter choice : ";
+        int c;
+        cin >> c;
+        if (c == 1)
+        {
+            create();
+            int wc = 0;
+            while (wc == 0)
+            {
+                cout << "1-> display the link list \n";
+                cout << "2-> delete node at the beginning \n";
+                cout << "3-> peek in queue \n";
+                cout << "4-> back the manu \n";
+                cout << "5-> exit \n";
+                cout << "enter choice : ";
+                int i;
+                cin >> i;
+                switch (i)
+                {
+                case 1:
+                    display();
+                    break;
+                case 2:
+                {
+            
+                    del();
+                    break;
+                }
+                case 3:
+                {
+
+                    peek();
+                    break;
+                }
+                
+                case 4:
+                    wc++;
+                    break;
+                case 5:
+                    goto exit;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            w++;
+        }
+    }
+exit:
+    return 0;
     
 }
